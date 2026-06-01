1e1a4f7861f8937985d063b70d5eeb893d6643a7
KINGALZO <152496786+KINGALZO@users.noreply.github.com>
2026-06-01 12:53:30 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
             <head><title>Projet DevOps</title></head>
             <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
                 <h1 style="color: #2c3e50;">Projet DevOps - Rapport Complet</h1>
-                <p style="font-size: 18px;">Application d'authentification de groupe déployée avec succès !</p>
+                <p style="font-size: 18px;">Application d'authentification de groupe déployée avec succès voilaa !</p>
                 <div style="margin-top: 30px; padding: 10px; background-color: #ecf0f1; display: inline-block; border-radius: 5px;">
                     <strong>Étudiants :</strong> Alamin & Membres du groupe
                 </div>
